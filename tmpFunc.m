5402c1b6a0d4d6af149ad688a77bf967e809c566
henryalps <henryalps@gmail.com>
2015-02-04 00:01:05 +0800

    完成了2-3的任务

--- tmpFunc.m
+++ tmpFunc.m
@@ -1,4 +1,4 @@
-function [dicNotchs,dicPeaks] = tmpFunc(data,peaks,notchs)
+function [dicNotchs,dicPeaks] = tmpFunc(data,peaks,onsets)
 %%预定义
 peaks=peaks(peaks(:,1)~=0,:);
 dicNotchs=peaks;
@@ -13,7 +13,9 @@ resampleInterval = 10;%减采样周期
 scale = 5;
 shiftFactor = 0.17;
 
-range = 200;%用距离法检测重博波的变化范围
+range = [200,0,0];%用距离法检测重博波的起始变化范围
+numOfZeroPassPoints = zeros(1,length(peaks(:,1))-1);%对每个心搏周期存放一个半周期小波变换过零点数量
+confactor = 3;%置信区间因子
 
 % %% 减采样
 % if resampleOrNot == 1 && length(data)>resampleInterval
@@ -106,7 +108,8 @@ for i=1:length(peaks(:,1))-1
 %   求小波变换
     wlp = waveletMethodB(dataPart);
 %   求小波变换的第4个过零点并增采样，作为降中峡位置
-    pos = findPassZeroPointPos(wlp) ;
+    pos = findPassZeroPointPos(wlp(1:floor(end/2))) ;    
+    numOfZeroPassPoints(i) = length(pos);
     pos =  pos(4) * resampleInterval + peaks(i);
 %   将降中峡位置增采样，并在[-resampleInterval,resampleInterval]内寻找二阶导最大值点，作为降中峡原始波形位置
     if pos + resampleInterval<=length(data) && pos - resampleInterval>0
@@ -114,12 +117,19 @@ for i=1:length(peaks(:,1))-1
             pos = pos + shift - resampleInterval; 
     end
 %   在[pos+1:pos+range-1]范围内寻找到包含[pos,data(pos)]与[pos+range,data(pos+range)]这两点的直线
-%       的距离最近的点，将其位置作为重博波位置
-    tmp = data(pos+1:pos+range-1);
-    [~,maxpos,~] = poinToLineDistance([pos+1:pos+range-1;tmp(:)']',...
-        [pos,data(pos)],[pos+range,data(pos+range)]);
+%       的距离最近的点，然后在起点到这点之间找波峰。如果找到了波峰，则将其位置作为重博波位置，否则使用终点位置
+    tmp = data(pos+1:pos+range(1)-1);
+    [~,maxpos,~] = poinToLineDistance([pos+1:pos+range(1)-1;tmp(:)']',...
+        [pos,data(pos)],[pos+range(1),data(pos+range(1))],1);
+    tmp = findPeakShiftInData(data(pos:pos+maxpos));
     dicNotchs(i,:) = [pos,data(pos)];
-    dicPeaks(i,:) = [pos+maxpos,data(pos+maxpos)];
+    if tmp ~= -1
+        dicPeaks(i,:) = [pos+tmp-1,data(pos+tmp-1)];
+    else
+        dicPeaks(i,:) = [pos+maxpos,data(pos+maxpos)];
+    end
+    %% 衰减法计算新的range
+    range = seqShifter(range,dicPeaks(i,1) - dicNotchs(i,1)+100,i);
 
 %%  将第4个过零点作为降中峡并绘图
 %     close all
@@ -133,9 +143,17 @@ for i=1:length(peaks(:,1))-1
 %         plot(dicNotchs(i,1),dicNotchs(i,2),'ro');
 %         plot(dicPeaks(i,1),dicPeaks(i,2),'go');
 %     end
+end
+%% 除去过零点数量在置信区间外的心搏周期notchs与peaks
+    numOfZeroPassPoints = numOfZeroPassPoints - mean(numOfZeroPassPoints);
+    var = std(numOfZeroPassPoints);
+    numOfZeroPassPoints = abs(numOfZeroPassPoints) < confactor*var;
+    dicNotchs = dicNotchs(numOfZeroPassPoints,:);
+    dicPeaks = dicPeaks(numOfZeroPassPoints,:);
+    for i = 1:length(dicNotchs(:,1))        
         plot(dicNotchs(i,1),dicNotchs(i,2),'ro');
         plot(dicPeaks(i,1),dicPeaks(i,2),'go');
-end
+    end
 end
 
     
